347ab19565284efccc62836bc95c38ef84980325
npetri13 <41867271+npetri13@users.noreply.github.com>
2024-07-20 18:11:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# shareReplay Test
+
+This small application shows one use case for the new @let directive
+
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check one opf these pages:
+
+- [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+- [Angular Docs](https://angular.dev/)
---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import { Component, inject, signal } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import 'zone.js';
+
+type Person = {
+  gender: string;
+};
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [AsyncPipe, JsonPipe],
+  template: `
+  <h1>ViewModel with AsyncPipe</h1>
+  <pre>{{ vmForAsyncPipe$ | async| json }}</pre>
+
+  <h1>With &commat; directive</h1>
+  @let femaleCount = (vmAsSignal()?.filter((person) => person.gender === 'Female'));
+  @let maleCount = (vmAsSignal()?.filter((person) => person.gender === 'Male'));
+  `,
+})
+export class App {
+  #httpClient = inject(HttpClient);
+  #request$ = this.#httpClient.get<Person[]>(
+    'https://random-data-api.com/api/v2/users?size=100'
+  );
+  visible = signal<boolean>(false);
+
+  vmForAsyncPipe$ = this.#request$.pipe(
+    map((data) => {
+      return {
+        femalesCount: data.filter((person) => person.gender === 'Female')
+          .length,
+        malesCount: data.filter((person) => person.gender !== 'Male').length,
+      };
+    })
+  );
+  vmAsSignal = toSignal(this.#request$);
+}
+
+bootstrapApplication(App, { providers: [provideHttpClient()] });
